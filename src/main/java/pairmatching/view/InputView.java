b35b53191e8e2a7a54d8f041310aad425ed65a11
taek2222 <oot309@daum.net>
2024-12-11 14:20:47 +0900

    feat: 과정, 레벨, 미션 선택 메시지 출력

--- src/main/java/pairmatching/view/InputView.java
+++ src/main/java/pairmatching/view/InputView.java
@@ -4,6 +4,15 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
+    public String readCurriculum() {
+        System.out.println("""
+            과정, 레벨, 미션을 선택하세요.
+            ex) 백엔드, 레벨1, 자동차경주
+            """);
+
+        return Console.readLine();
+    }
+
     public String readSelectFunction() {
         return Console.readLine();
     }
